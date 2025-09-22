8cad294b90aee2b0181261b20f2178075a49cbf1
lasha <lashatatu@gmail.com>
2025-09-22 20:33:20 +0400

    sortby partial inplementation

--- src/Components/ProductsPageComponent.jsx
+++ src/Components/ProductsPageComponent.jsx
@@ -1,18 +1,21 @@
 import React, { useState, useRef } from "react";
 import { Link } from "@tanstack/react-router";
 import PriceFilterComponent from "./PriceFilterComponent.jsx";
+import ProductsSortByComponent from "./ProductsSortByComponent.jsx";
 
 const ProductsPageComponent = ({ products }) => {
   const [showFilter, setShowFilter] = useState(false);
+  const [showSortBy, setShowSortBy] = useState(false);
   const filterRef = useRef(null);
+  const sortByRef = useRef(null);
 
   return (
     <div className='px-28'>
       <div className='flex justify-between py-12'>
         <h1 className='text-4xl font-bold'>Products</h1>
         <div className='flex items-center space-x-4'>
-          <PriceFilterComponent filterRef={filterRef} setShowFilter={setShowFilter} showFilter={showFilter}/>
-          <button>sort by</button>
+          <PriceFilterComponent filterRef={filterRef} setShowFilter={setShowFilter} showFilter={showFilter} />
+          <ProductsSortByComponent sortByRef={sortByRef} setShowSortBy={setShowSortBy} showSortBy={showSortBy} />
         </div>
       </div>
       <div className='grid grid-cols-4 gap-4'>
---
+++ src/Components/ProductsSortByComponent.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const ProductsSortByComponent = ({
+  setShowSortBy,
+  showSortBy,
+  sortByRef
+}) => {
+  return (
+    <div className='relative' ref={sortByRef}>
+      <button
+        className='flex items-center space-x-2 text-gray-600'
+        onClick={() => setShowSortBy((prev) => !prev)}
+        type='button'
+      >
+        <span>Sort By</span>
+      </button>
+      {showSortBy && (
+        <div className='absolute right-0 mt-2 w-50 bg-white rounded-lg shadow-lg border border-gray-200 z-50 p-4'>
+          <h1 className='font-semibold py-2'>Sort By</h1>
+          <div className='cursor-pointer'>
+            <p className='py-2'>new products first</p>
+            <p className='py-2'>price, low to high</p>
+            <p className='py-2'>price, high to low</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default ProductsSortByComponent;
