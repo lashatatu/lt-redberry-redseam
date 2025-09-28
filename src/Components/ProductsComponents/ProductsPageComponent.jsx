4e24ebb74c91da73e98ffba88db425c5cbf4395c
lasha <lashatatu@gmail.com>
2025-09-28 04:58:22 +0400

    fixed sort by used headlesui

--- src/Components/ProductsComponents/ProductsPageComponent.jsx
+++ src/Components/ProductsComponents/ProductsPageComponent.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import ProductsPriceFilterComponent from "./ProductsPriceFilterComponent.jsx";
 import ProductsSortByComponent from "./ProductsSortByComponent.jsx";
 import ProductsPaginationComponent from "./ProductsPaginationComponent.jsx";
@@ -7,33 +7,24 @@ import ProductsItemCardComponent from "./ProductsItemCardComponent.jsx";
 const ProductsPageComponent = ({ products, sort, price_from, price_to, onSort }) => {
   const [showFilter, setShowFilter] = useState(false);
   const [showSortBy, setShowSortBy] = useState(false);
-  const filterRef = useRef(null);
-  const sortByRef = useRef(null);
 
-  const handleToggleFilter = () => {
-    setShowFilter((prev) => {
-      if ( !prev ) {
-        setShowSortBy(false);
-      }
-      return !prev;
-    });
+  const handleFilterDialog = (open) => {
+    setShowFilter(open);
+    if (open) setShowSortBy(false);
   };
 
-  const handleToggleSortBy = () => {
-    setShowSortBy((prev) => {
-      if ( !prev ) {
-        setShowFilter(false);
-      }
-      return !prev;
-    });
+  const handleSortByDialog = (open) => {
+    setShowSortBy(open);
+    if (open) setShowFilter(false);
   };
+
   return (
     <div className='px-25'>
       <div className='flex justify-between py-12'>
         <h1 className='text-4xl font-bold'>Products</h1>
         <div className='flex items-center space-x-4'>
-          <ProductsPriceFilterComponent filterRef={filterRef} showFilter={showFilter} onToggle={handleToggleFilter} onSort={onSort} sort={sort} price_from={price_from} price_to={price_to} />
-          <ProductsSortByComponent sortByRef={sortByRef} showSortBy={showSortBy} onToggle={handleToggleSortBy} onSort={onSort} sort={sort} price_from={price_from} price_to={price_to} />
+          <ProductsPriceFilterComponent open={showFilter} onClose={handleFilterDialog} onSort={onSort} sort={sort} price_from={price_from} price_to={price_to} />
+          <ProductsSortByComponent open={showSortBy} onClose={handleSortByDialog} onSort={onSort} sort={sort} price_from={price_from} price_to={price_to} />
         </div>
       </div>
       <ProductsItemCardComponent products={products}/>
--- src/Components/ProductsComponents/ProductsPriceFilterComponent.jsx
+++ src/Components/ProductsComponents/ProductsPriceFilterComponent.jsx
@@ -1,46 +1,53 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { HiOutlineAdjustmentsHorizontal } from "react-icons/hi2";
+import { Dialog } from "@headlessui/react";
 
-const ProductsPriceFilterComponent = ({filterRef,showFilter,onToggle,onSort,sort,price_from,price_to}) => {
+const ProductsPriceFilterComponent = ({
+  open,
+  onClose,
+  onSort,
+  sort,
+  price_from,
+  price_to
+}) => {
   const [priceFrom, setPriceFrom] = useState(price_from || "");
   const [priceTo, setPriceTo] = useState(price_to || "");
   return (
-    <div className='relative' ref={filterRef}>
+    <div className='relative'>
       <button
         className='flex items-center space-x-2 text-gray-600'
-        onClick={onToggle}
+        onClick={() => onClose( !open)}
         type='button'
       >
-        <HiOutlineAdjustmentsHorizontal size={20} from={'black'}/>
+        <HiOutlineAdjustmentsHorizontal size={20} from={"black"} />
         <span>filter</span>
       </button>
-      {showFilter && (
-        <div className="absolute right-0 mt-2 w-100 bg-white rounded-lg shadow-lg border border-gray-200 z-50 p-4" >
-          <div className="mb-4">
-            <span className="block font-semibold text-lg text-gray-900 mb-2">Select price</span>
-            <div className="flex space-x-2 mb-2">
-              <input
-                placeholder="From *"
-                className="w-1/2 border border-gray-300 rounded-md px-3 py-2 text-sm"
-                value={priceFrom}
-                onChange={e => setPriceFrom(e.target.value)}
-              />
-              <input
-                placeholder="To *"
-                className="w-1/2 border border-gray-300 rounded-md px-3 py-2 text-sm"
-                value={priceTo}
-                onChange={e => setPriceTo(e.target.value)}
-              />
-            </div>
-            <button
-              className="bg-orange-600 text-white py-2 px-10 rounded-md font-medium mt-2 transition-colors hover:bg-orange-700 cursor-pointer float-right"
-              onClick={() => onSort(sort, priceFrom, priceTo)}
-            >
-              Apply
-            </button>
+      <Dialog open={open} onClose={() => onClose(false)} className='fixed inset-0 z-50 flex items-start justify-end'>
+        <div className='fixed inset-0 bg-black/10' aria-hidden='true' />
+        <Dialog.Panel className='absolute w-100 right-28 top-54 mt-2 bg-white rounded-lg shadow-lg border border-gray-200 z-50 p-4'>
+          <span className='block font-semibold text-lg text-gray-900 mb-2'>Select price</span>
+          <div className='flex space-x-2 mb-2'>
+            <input
+              placeholder='From *'
+              className='w-1/2 border border-gray-300 rounded-md px-3 py-2 text-sm'
+              value={priceFrom}
+              onChange={e => setPriceFrom(e.target.value)}
+            />
+            <input
+              placeholder='To *'
+              className='w-1/2 border border-gray-300 rounded-md px-3 py-2 text-sm'
+              value={priceTo}
+              onChange={e => setPriceTo(e.target.value)}
+            />
           </div>
-        </div>
-      )}
+          <button
+            className='bg-orange-600 text-white py-2 px-10 rounded-md font-medium mt-2 transition-colors hover:bg-orange-700 cursor-pointer float-right'
+            onClick={() => onSort(sort, priceFrom, priceTo)}
+          >
+            Apply
+          </button>
+        </Dialog.Panel>
+      </Dialog>
     </div>
   );
 };
